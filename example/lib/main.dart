5e5bb2fdaec4ec89196207bc9c206259df289512
oussama berhili <oussamaberhili190@gmail.com>
2025-05-19 13:43:25 +0100

    format

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
-
 import 'package:custom_scaffold_plus/custom_scaffold_plus.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
